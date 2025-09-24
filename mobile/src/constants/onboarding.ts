053a1bf5ce2dca2796caa8c3639d80cc67e71970
2hanse <prsuw113@naver.com>
2025-09-25 03:01:57 +0900

    ✨feat: 온보딩 페이지1 구현

---
+++ mobile/src/constants/onboarding.ts
@@ -0,0 +1,29 @@
+const TIMINGS_DURATION = 300;
+const STAGGER_DELAY = 200;
+const ANIMATION_START_DELAY = 500;
+
+const FEATURES_TEXTS = [
+  { id: "point1", text: "일정에 맞는 맞춤 학습 콘텐츠" },
+  { id: "point2", text: "바쁜 일상 속 자투리 시간 완벽 활용" },
+  { id: "point3", text: "유명 강연을 내 상황에 맞게 큐레이션" },
+];
+
+const SLOGANS_TEXTS = {
+  one: "오늘 일정에 맞춰,",
+  two: "내게 꼭 필요한 코멘트를 !",
+  sub: "성장이 습관이 되는 가장 쉬운 방법",
+};
+
+const DESCRIPTION = [
+  { text: "구글 캘린더와 연동해서 TED, 세바시 등\n" },
+  { text: "유명 강연을 당신의 일정에 맞춰 추천해드려요." },
+];
+
+export {
+  TIMINGS_DURATION,
+  STAGGER_DELAY,
+  ANIMATION_START_DELAY,
+  FEATURES_TEXTS,
+  SLOGANS_TEXTS,
+  DESCRIPTION,
+};
